7a8457377cb5ca4497988504be744261b03989bd
Matthew McCaskill <mattrmccaskill@gmail.com>
2023-04-26 15:13:37 -0500

    fix CI build

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
